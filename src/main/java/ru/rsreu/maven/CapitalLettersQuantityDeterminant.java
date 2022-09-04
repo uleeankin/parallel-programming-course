7667961762ed7d1bbfc8a0bbeaf37b33eeb888e9
uleeankin <sokolowa.yu.a@gmail.com>
2022-09-04 16:18:39 +0300

    Initial commit

---
+++ src/main/java/ru/rsreu/maven/CapitalLettersQuantityDeterminant.java
@@ -0,0 +1,9 @@
+package ru.rsreu.maven;
+
+public class CapitalLettersQuantityDeterminant
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ru/rsreu/maven/CapitalLettersQuantityDeterminantTest.java
@@ -0,0 +1,20 @@
+package ru.rsreu.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CapitalLettersQuantityDeterminantTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
